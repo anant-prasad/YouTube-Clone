7961e0b75b780dd28210c936e457df27c00c885d
Anant Prasad <anant29october@gmail.com>
2020-06-30 16:51:13 +0530

    web View

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,46 +1,58 @@
 import React from "react";
 import { View, Image, Text, Dimensions } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Card(props) {
+  const navigation = useNavigation();
   return (
-    <View style={{ marginBottom: 3 }}>
-      <Image
-        source={{
-          uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
-        }}
-        style={{
-          width: "100%",
-          height: 200,
-        }}
-      />
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          margin: 5,
-        }}
-      >
-        <MaterialIcons name="account-circle" size={45} color="#212121" />
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("videoplayer", {
+          videoId: props.videoId,
+          title: props.title,
+        })
+      }
+    >
+      <View style={{ marginBottom: 3 }}>
+        <Image
+          source={{
+            uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
+          }}
+          style={{
+            width: "100%",
+            height: 200,
+          }}
+        />
         <View
           style={{
-            marginLeft: 3,
+            flexDirection: "row",
+            alignItems: "center",
+            margin: 5,
           }}
         >
-          <Text
+          <MaterialIcons name="account-circle" size={45} color="#212121" />
+          <View
             style={{
-              fontSize: 15,
-              fontWeight: "bold",
-              width: Dimensions.get("screen").width - 70,
+              marginLeft: 3,
             }}
-            ellipsizeMode="tail"
-            numberOfLines={2}
           >
-            {props.title}
-          </Text>
-          <Text>{props.channel}</Text>
+            <Text
+              style={{
+                fontSize: 15,
+                fontWeight: "bold",
+                width: Dimensions.get("screen").width - 70,
+              }}
+              ellipsizeMode="tail"
+              numberOfLines={2}
+            >
+              {props.title}
+            </Text>
+            <Text>{props.channel}</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -1,31 +1,42 @@
 import React from "react";
-import { View, Text, Dimensions, Image } from "react-native";
+import { View, Text, Dimensions, Image, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function MiniCard(props) {
+  const navigation = useNavigation();
   return (
-    <View style={{ flexDirection: "row", margin: 10, marginBottom: 0 }}>
-      <Image
-        source={{
-          uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
-        }}
-        style={{
-          width: "40%",
-          height: 80,
-        }}
-      />
-      <View style={{ paddingLeft: 7 }}>
-        <Text
-          style={{
-            fontSize: 15,
-            width: Dimensions.get("screen").width / 2,
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("videoplayer", {
+          videoId: props.videoId,
+          title: props.title,
+        })
+      }
+    >
+      <View style={{ flexDirection: "row", margin: 10, marginBottom: 0 }}>
+        <Image
+          source={{
+            uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
           }}
-          ellipsizeMode="tail"
-          numberOfLines={3}
-        >
-          {props.title}
-        </Text>
-        <Text style={{ fontSize: 12 }}>{props.channel}</Text>
+          style={{
+            width: "40%",
+            height: 80,
+          }}
+        />
+        <View style={{ paddingLeft: 7 }}>
+          <Text
+            style={{
+              fontSize: 15,
+              width: Dimensions.get("screen").width / 2,
+            }}
+            ellipsizeMode="tail"
+            numberOfLines={3}
+          >
+            {props.title}
+          </Text>
+          <Text style={{ fontSize: 12 }}>{props.channel}</Text>
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
